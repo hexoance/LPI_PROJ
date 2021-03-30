14850f705300e6a08506c481dd4235501280d46d
Jorge Lopes <38607@ufp.edu.pt>
2021-03-30 20:09:00 +0100

    Feito Balanceamento de Ficheiros Input (.wav)
    Commit contém a implementação com ordem aleatória em relação ao número de ficheiros da classe com o menor número de ficheiros (balanceamento).

--- retrain-models/convert_fsd50k.py
+++ retrain-models/convert_fsd50k.py
@@ -4,15 +4,28 @@ from pydub import AudioSegment
 import random
 
 DATASETS_PATH = './datasets/'
-#DATASETS_PATH ='D:/datasets/'
+#DATASETS_PATH = 'D:/datasets/'
 DATASET = 'FSD50k/'
-classes = ['Computer_keyboard', 'Knock', 'Scissors']
-ids = {'Computer_keyboard': 42, 'Knock': 106, 'Scissors': 143}
-filesInClasses = {}
+classes = []
+ids = {}
 mappings = []
 
-for i in range(len(classes)):
-    filesInClasses[classes[i]] = 0
+def readClasses(file):
+    with open(DATASETS_PATH + DATASET + file, newline='') as f:
+        reader = csv.reader(f)
+        iterreader = iter(reader)
+        next(iterreader)
+        for row in iterreader:
+            classes.append(row[0])
+
+        print(classes)
+
+def readVocabulary(file):
+    with open(DATASETS_PATH + DATASET + file, newline='') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            ids[row[1]] = row[0]
+
 
 def extract_mappings(file):
     with open(DATASETS_PATH + DATASET + file, newline='') as f:
@@ -29,20 +42,18 @@ def extract_mappings(file):
                     if row[3] == "train":
                         fold = 1
                 mappings.append([row[0] + ".wav", str(fold), str(ids[values[0]]), values[0]])
-                filesInClasses[values[0]] += 1
-        print(filesInClasses)
 
 
 def save_mappings_to_csv(maps):
-    min = 9999
-    for classNames in filesInClasses:
-        if filesInClasses[classNames] < min:
-            min = filesInClasses[classNames]
+    with open(DATASETS_PATH + DATASET + 'data_mapping.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["filename", "fold", "target", "category"])
+        for mapping in maps:
+            writer.writerow(mapping)
 
-    countClasses = {}
-    for i in range(len(classes)):
-        countClasses[classes[i]] = 0
 
+def balanceWavFiles(maps):
+    # Classe -> Num. total de ficheiros .wav dessa classe (count) -> Array os ficheiros .wav (maps)
     classes_maps = {}
     for mapping in maps:
         category = mapping[3]
@@ -52,29 +63,20 @@ def save_mappings_to_csv(maps):
         classes_maps[category]['count'] += 1
         classes_maps[category]['maps'].append(mapping)
 
-    print(classes_maps)
+    print("\nClasses: \n", classes_maps)
 
-    with open(DATASETS_PATH + DATASET + 'data_mapping.csv', 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(["filename", "fold", "target", "category"])
-        for mapping in maps:
-            wav = mapping[0]
-            className = mapping[3]
-            if countClasses[className] < min and not checkNonRepeatableWav(wav):
-                writer.writerow(mapping)
-                countClasses[className] += 1
-        print(countClasses)
+    # Número de ficheiros da Classe com menos ficheiros
+    min_count = min(val['count'] for key, val in classes_maps.items())
+    print("\nClasses to Balance (MIN):", min_count)
 
-def checkNonRepeatableWav(wav):
-    with open(DATASETS_PATH + DATASET + 'data_mapping.csv', 'r', newline='') as file:
-        reader = csv.reader(file)
-        exist = False
-        for row in reader:
-            if row[0] == wav:
-                print("Repeated:", row[0])
-                exist = True
-                break
-    return exist
+    # Obter os MIN ficheiros .wav aleatórios de cada classe
+    maps = []
+    for category in classes_maps:
+        for line in random.sample(classes_maps[category]['maps'], min_count):
+            maps.append(line)
+
+    print("\nReady to Write:\n", {'Balanced Classes:': maps})
+
 
 def copy_matching_files(maps):
     for mapping in maps:
@@ -92,8 +94,10 @@ def copy_matching_files(maps):
 
         audio.export(DATASETS_PATH + DATASET + "audio/" + mapping[0], format="wav")
 
-
+readClasses('classes.csv')
+readVocabulary('vocabulary.csv')
 extract_mappings('dev.csv')
 extract_mappings('eval.csv')
+balanceWavFiles(mappings)
 save_mappings_to_csv(mappings)
 copy_matching_files(mappings)
